76a1c327ef70fc0de2564ebfff000fef19d8e8ae
soldisn2025-png <soldisn2025@gmail.com>
2026-03-15 13:01:00 -0400

    Fix VB-MAPP listener and independent play data

--- server/src/db/migrate.js
+++ server/src/db/migrate.js
@@ -8,7 +8,14 @@ async function migrate() {
 
   const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
   try {
-    const files = ['schema.sql', 'seeds.sql', 'vbmapp_seeds.sql', 'vbmapp_program_templates.sql', 'import_audit_log.sql'];
+    const files = [
+      'schema.sql',
+      'seeds.sql',
+      'vbmapp_seeds.sql',
+      'vbmapp_program_templates.sql',
+      'vbmapp_program_templates_overrides.sql',
+      'import_audit_log.sql',
+    ];
     for (const file of files) {
       const sql = fs.readFileSync(path.join(__dirname, file), 'utf8');
       await pool.query(sql);
